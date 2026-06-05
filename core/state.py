cb4fc6acd1e54c4fe5f8aa3447fac09ccf55cec9
Linu Anil Teresa <78141612+linuteresa@users.noreply.github.com>
2026-06-05 11:18:14 -0400

    added pydantic schema

--- core/state.py
+++ core/state.py
@@ -2,6 +2,8 @@ from typing import List, TypedDict, Annotated, Tuple
 import operator
 
 class PlanExecuteState(TypedDict):
+    input: str
     plan: List[str]
     past_steps: Annotated[List[Tuple[str, str]],operator.add]
-    response:str
+    response: str
+    confidence_score: float
